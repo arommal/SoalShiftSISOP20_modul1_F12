87ff0d0e90d72912a02a1c10166661029b70cd0b
arommal <rosalammora@gmail.com>
2020-02-21 15:40:23 +0700

    Add existing files

--- 2/no2.sh
+++ 2/no2.sh
@@ -1,3 +1,2 @@
-nama=""
-read nama
+nama=$1
 cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 28 > $nama.txt
--- dlscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {001..028}; do
- wget -o /wget.log http://loremflickr.com/320/240/cat/"pdkt_kusuma_$i".jpg
-done
-
